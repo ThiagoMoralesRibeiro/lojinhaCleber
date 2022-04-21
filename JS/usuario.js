58cb4ac05cad702fb52cdcb86a961a3be6749ef4
ThiagoMRibeiro05 <thiagomoralesribeiro@gmail.com>
2022-04-20 23:39:07 -0300

    bhb

--- JS/usuario.js
+++ JS/usuario.js
@@ -67,7 +67,7 @@ $(document).on('click', "#btn-confirma", function () {
   $.post(
    vUrl, //variável correspondente a meu arquivo php
    vData,
-   function(response,status)
+   function(response, status)
    {
      // tratando o status de retorno. Sucesso significa que o envio e retorno foi executado com sucesso.
      if(status == "success")
