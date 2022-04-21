569cc0a5682521e1fd7e0151aaca4911339772bc
ThiagoMRibeiro05 <thiagomoralesribeiro@gmail.com>
2022-04-21 00:07:13 -0300

    ygggygy

--- usuarios.php
+++ usuarios.php
@@ -16,7 +16,7 @@ $foto=$_REQUEST['foto'];
 
 $user = new Usuarios($email, $senha, $nome, $cpf, $endereco, $bairro, $cidade, $uf, $cep, $telefone, $foto);
 
- json_encode(
+echo json_encode(
     array(
         "email" => $email,
         "nome" => $nome,
