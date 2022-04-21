380cf66ba67f9ed08ca4d9aa5857783f6cf28630
ThiagoMRibeiro05 <thiagomoralesribeiro@gmail.com>
2022-04-20 23:46:32 -0300

    jkhbbhj

--- JS/usuario.js
+++ JS/usuario.js
@@ -69,9 +69,8 @@ $(document).on('click', "#btn-confirma", function () {
    vData,
    function(response, status)
    {
-     // tratando o status de retorno. Sucesso significa que o envio e retorno foi executado com sucesso.
-     if(status == "success")
-     {
+        // tratando o status de retorno. Sucesso significa que o envio e retorno foi executado com sucesso.
+     
         // pegando os dados jSON
         var obj = jQuery.parseJSON(response);
  
@@ -88,7 +87,7 @@ $(document).on('click', "#btn-confirma", function () {
             "Telefone"+obj.telefone+ "<br>"+
             "Foto"+obj.foto
        );
-     }    	
-   }
+    }    	
+   
   );
 });
