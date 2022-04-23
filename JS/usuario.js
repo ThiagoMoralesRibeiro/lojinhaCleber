95686bbdd1fa8ae32874694309c163df9e6a06cb
ThiagoMRibeiro05 <thiagomoralesribeiro@gmail.com>
2022-04-22 21:48:55 -0300

    bhghc

--- JS/usuario.js
+++ JS/usuario.js
@@ -45,4 +45,55 @@
 */
 
 
+$(document).on('click', "#btn-confirma", function () {
+	
+  // pegando os dados
+        nome = $("#name").val();
+        email = $("#email").val();
+        pwd = $("#pwd").val();
+        cpf = $("#cpf").val();
+        adress = $("#adress").val();
+        bairro = $("#bairro").val();
+        cidade = $("#cidade").val();
+        uf = $("#uf").val();
+        cep = $("#cep").val();
+        telefone = $("#telefone").val();
+        foto = $("#foto").val();
 
+        
+	
+  // criando as variáveis
+  var vUrl = "usuarios.php";
+  var vData = { email: email, nome: nome , senha : pwd , cpf : cpf , adress : adress , bairro : bairro , cidade : cidade, uf : uf , cep : cep , telefone : telefone , foto : foto  };
+ 
+  $.post(
+      
+   vUrl, //variável correspondente a meu arquivo php
+   vData,
+   function (response, status)
+   {
+     // tratando o status de retorno. Sucesso significa que o envio e retorno foi executado com sucesso.
+     if(status == "success")
+     {
+        // pegando os dados jSON
+        var obj = jQuery.parseJSON(response);
+        
+       $("#result").html(
+            "Nome:" +obj.nome + "<br>" +
+            "Email:"+obj.email + "<br>"+
+            "Senha:"+obj.senha + "<br>"+
+            "CPF:"+obj.cpf + "<br>"+
+            "Endereço:"+obj.adress + "<br>"+
+            "Bairro:"+obj.bairro + "<br>"+
+            "Cidade:"+obj.cidade + "<br>"+
+            "UF:"+obj.uf + "<br>"+
+            "CEP:"+obj.email + "<br>"+
+            "Telefone"+obj.telefone+ "<br>"+
+            "Foto"+obj.foto+  alert(obj.nome)
+
+           
+       );
+     }    	
+   }
+  );
+});
