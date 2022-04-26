7cf9b9ebe4cd7a737dc306fb31a9f4297388e60c
ThiagoMRibeiro05 <thiagomoralesribeiro@gmail.com>
2022-04-26 01:02:27 -0300

    yui

--- nivelUsuario.php
+++ nivelUsuario.php
@@ -15,13 +15,42 @@
     
     <!-- Latest compiled JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+	<!-- Lastest compiled AJAX-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         
 </head>
 <body>
 
+	<table border="2px" cellpadding="7px" cellspacing="2">
+						<tr>
+							<td>Nível do Usuário</td>
+							
+						</tr>
+		<?php    
+				include_once './classes/dataBaseConnection.class.php';
+		
+				$consultaUsuarios = new DataBaseConnection();
+				$resultSelect = $consultaUsuarios->getConn()->query("SELECT * FROM nivelusuarios");
+
+				while($line = mysqli_fetch_assoc($resultSelect)){
+					$nivelUser =  $line['nivel'] ;
+					
+				}
+				echo
+					"<tr>
+						<td>$nivelUser</td>
+						
+					</tr>";
+
+
+		?>
+
+	</table>
+
 	<div class="container m-5 p-5">
   		<h2>Insert form</h2>
-  		<form action="PHP/nivelUser.php" method="post">
+  		<form action="" method="post" id="nivelUserCad">
   			<div class="form-group">
       			<label for="userLevel">Nível do Usuário:</label>
       			<input type="number" class="form-control" id="userLevel" placeholder="Enter nivel usuario" name="userLevel">
@@ -33,9 +62,33 @@
         		</label>
      	 	
     		</div>
-    		<button type="submit" class="btn btn-dark">Submit</button>
+    		<button type="submit" class="btn btn-dark" id="btn-confirma">Submit</button>
   		</form>
 	</div>
+	<script type="text/javascript">
+		$(document).ready(function() {
+    
+			$('#btn-confirma').click(function() {
+        		info = $('#nivelUserCad').serialize();
+        		console.log(info);
+
+				$.ajax({
+					type: 'POST',
+					dataType: 'json',
+					url: 'saveNivelUser.php',
+					async: true,
+					data: info,
+					success: function(response) {
+						location.reload();
+					}
+				});
+
+				return false;
+			});
+
+		});
+
+	</script>
 
 </body>
 </html>
--- saveNivelUser.php
+++ saveNivelUser.php
@@ -1,10 +1,12 @@
 <?php
 
-include_once '../classes/nivelUsuarios.class.php';
+include_once './classes/nivelUsuarios.class.php';
 
 $nivel = $_POST['userLevel'];
 
 $nivelUser = new NivelUsuarios($nivel);
+$response = array('success'=>true);
+echo json_encode($response);
 
 
 
